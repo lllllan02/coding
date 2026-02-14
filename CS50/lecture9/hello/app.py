22de49231feb7227e5954e2f8dee7a5176e56349
lllllan <342310798@qq.com>
2026-02-14 18:34:05 +0800

    Add initial Flask application for CS50 lecture 9, including setup instructions, main app logic, and HTML templates for greeting users.

---
+++ CS50/lecture9/hello/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index(): 
+    if request.method == "POST":
+        name = request.form.get("name", "World")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
